cd0a5bd0a9acd19a851b8a6ef3fd7ef10c1ce8c4
paulodder <paul_lodder@live.nl>
2017-12-22 17:21:19 +0100

    Switch to python 3 (finallY

---
+++ genes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Gene(models.Model):
+    """Represents a gene and its expression values"""
+    univ_name = models.CharField(max_length=10)
+    # name = models.CharField(max_length=8)
+    Jan_expr = models.FloatField(default=0)
+    GTE_expr = models.FloatField(default=0)
+    TCGAN_expr = models.FloatField(default=0)
+    
