6dbf05a1b2f4c31eea07e8fb23f7eee3eb845698
paulodder <paul_lodder@live.nl>
2018-01-02 09:34:15 +0100

    Write CSV download in alphabetic order, use fu full_gene_rows.txt

--- genes/views.py
+++ genes/views.py
@@ -28,11 +28,13 @@ def base(request):
 
     # template = loader.get_template('genes/base.html' )
     # response = HttpResponse(template.render))    
-    # response['age'] = 120 
+    # response['age'] = 120
+    # print("AM i here?")
     return render(request, 'genes/base.html')
 
 
 def filter_gene_indices(request):
+    print("Here, request taken")
     conds_str = request.GET["conds"]
     conds_str = conds_str.replace('13', '20') # In case 13 is present, accepts any
     # gene with expression lower than 20 (max possible gene expression in
@@ -98,10 +100,12 @@ def generate_csv(request):
     ct_db_member = Counter(brug_wang)
     wang_member = Counter(brug_ct_db)
 
-    brug_sorted = sorted(brug_genes, key=lambda g: (wang_member[g],
-                                                    ct_db_member[g]))
+    # brug_sorted = sorted(brug_genes, key=lambda g: (wang_member[g],
+    #                                                 ct_db_member[g]))
+    # sort based on name
+    brug_sorted = sorted(brug_genes, key=lambda g: g.rev_ann)
     print(len(brug_sorted))
-    with open('gene_rows_fulldata.txt', 'r') as f:
+    with open('full_gene_rows.txt', 'r') as f:
         name_row_dic = json.load(f)
     writer.writerow(name_row_dic['HEADER'])
     for g in brug_sorted:
