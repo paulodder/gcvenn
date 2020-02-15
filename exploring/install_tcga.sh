64148b6f460e36e4467ce7079bd85896dcf18fe0
Philip Hartout <philip.hartout@protonmail.com>
2020-02-15 16:38:19 +0100

    :memo: remove emacs typoe

--- exploring/install_tcga.sh
+++ exploring/install_tcga.sh
@@ -1,4 +1,4 @@
 wget -P $SRC_DIR https://drive.google.com/file/d/0BwAUopGWU_khN2ZlaDdlSk5HUzQ/edit?usp=sharing
 tar zxf TCGA_UUID-1.0.tar.gz
 cd TCGA_UUID-1.0
-python setup.py installXF
+python setup.py install
